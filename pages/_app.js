bc0a9c5f020195bfa34aa2e9e7f88b84ca95010b
Ashlone <ashchibvuri@gmail.com>
2021-11-18 04:03:56 +0200

    final changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,9 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
-import Script from "next/script";
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-G0QTTEX7ME`}
-      />
-
-      <Script strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-G0QTTEX7ME');
-        `}
-      </Script>
       <Component {...pageProps} />
     </Layout>
   );
